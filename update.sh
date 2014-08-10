47a0c613a07916334757ba365678e8ddff33cde1
Marek Ziółkowski <ziolek@users.noreply.github.com>
2014-08-10 12:20:43 +0200

    Cleaning

--- update.sh
+++ update.sh
@@ -41,26 +41,3 @@ cp $SRC_DIR/*.tdb $PRODUCT_DIR
 cp $SRC_DIR/*.mdx $GMAP_DIR
 
 echo_green "Conversion finished."
-
-
-exit 0
-declare gmapdir="gmap/"
-declare productdir=$gmapdir"Product1/"
-declare sourcedir="./map/"
-
-for imgfile in $sourcedir*.img
-do
-	bname=`basename $imgfile`
-	if [[ $imgfile = *mdr* ]] ; then
-		dir=$gmapdir${bname%.img}
-	else
-		dir=$productdir${bname%.img}
-	fi
-	rm -rf $dir
-	mkdir -p $dir
-	./gmt -g -o $dir $imgfile
-done
-cp $sourcedir/*.tdb $productdir
-cp $sourcedir/*.mdx $gmapdir
-
-echo "Aktualizacja zakończona."
